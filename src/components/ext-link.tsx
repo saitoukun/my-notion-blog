2134e200afa1083480b0e3c2da2a5bcf70105c6c
saitoukun <saitoukun13@gmail.com>
2020-05-06 13:25:56 +0900

    add profile

--- src/components/ext-link.tsx
+++ src/components/ext-link.tsx
@@ -1,3 +1,11 @@
 export default props => (
-  <a {...props} rel="noopener" target={props.target || '_blank'} />
+  <>
+    <a {...props} rel="noopener" target={props.target || '_blank'} />
+    <style jsx>{`
+      a {
+        font-size: 0.75rem;
+        padding: 5px;
+      }
+    `}</style>
+  </>
 )
